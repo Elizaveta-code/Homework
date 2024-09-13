cba4a01a63277c6d4f62db4dd6f88dd2f300690d
Elizaveta-code <elizavetaragozina69@gmail.com>
2024-09-13 16:25:25 +0300

    first commit

---
+++ 1 week/code1.py
@@ -0,0 +1,40 @@
+'''a=int(input('Input first number '))
+b=int(input('Input second number '))
+c=(a**2+b**2)**0.5
+print('Гипотенуза = ', c)
+print(2+2)'''
+'''a=int(input('Input first number '))
+b=int(input('Input second number '))
+print(a+b, a*b, a/b, sep='\n')
+a=int(input('Input number '))
+print(a%10)
+a='Hello'
+print(a[1:3], a[4], sep='\n')
+print(a.find('e'))
+print(a.replace('l', 'L'))
+print(a.count('l', 1, 3))
+print(a[1:3].count('l'))
+b=[1,4,6,7]
+b.append(3)
+print(b)
+n=[[0]*10]*10
+input().split(',')
+print(n)
+a=list(map(int(input().split()))
+       res = 1
+for i in a
+       res *= i 
+       print(res**(1/len(a)))
+f=open('pp.txt')
+f.read()
+f.close()
+f.readline()'''
+f=open('pp.txt', 'r')
+lines=f.readlines()
+a=list(map(int,f.readlines[0].split()))
+op=lines(1)
+if op=='+':
+    res = sum(a)
+elif op == '-':
+     
+
